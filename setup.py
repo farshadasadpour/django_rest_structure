9fdb7e70f1c331827d7b281be56effc8eab0d45e
Aref Mousakhani <48289801+ArefMousakhani@users.noreply.github.com>
2021-11-09 12:10:31 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-rest-structure',
-    version='1.1.1',
+    version='1.1.2',
     packages=find_packages(),
     include_package_data=True,
     data_files=[('locales', [f'django_rest_structure/translations/locales/{x}' for x in
