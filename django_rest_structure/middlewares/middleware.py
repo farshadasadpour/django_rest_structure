cb159d239d64931829f66f500d41f5c6532e4aca
Aref Mousakhani <48289801+ArefMousakhani@users.noreply.github.com>
2021-11-23 14:43:26 +0330

    Update middleware.py

--- django_rest_structure/middlewares/middleware.py
+++ django_rest_structure/middlewares/middleware.py
@@ -12,6 +12,7 @@ class RequestHandlerMiddleware:
     def __call__(self, request):
         request.request_uid = uuid.uuid4()
         request_time = timezone.now()
+        request.body
 
         response = self.get_response(request)
         REST_STRUCTURE_CONF['log_handler'](request, response, response.err if hasattr(response, 'err') else None,
